e2d3fa084c4fd6492059a9d5e60acd1e25288eb6
siddiq9080 <muhamad89888@gmail.com>
2024-05-31 17:20:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// Start the countdown
+setTimeout(() => {
+  document.getElementById("countdown").innerText = 10;
+
+  setTimeout(() => {
+    document.getElementById("countdown").innerText = 9;
+
+    setTimeout(() => {
+      document.getElementById("countdown").innerText = 8;
+
+      setTimeout(() => {
+        document.getElementById("countdown").innerText = 7;
+
+        setTimeout(() => {
+          document.getElementById("countdown").innerText = 6;
+
+          setTimeout(() => {
+            document.getElementById("countdown").innerText = 5;
+
+            setTimeout(() => {
+              document.getElementById("countdown").innerText = 4;
+
+              setTimeout(() => {
+                document.getElementById("countdown").innerText = 3;
+
+                setTimeout(() => {
+                  document.getElementById("countdown").innerText = 2;
+
+                  setTimeout(() => {
+                    document.getElementById("countdown").innerText = 1;
+
+                    // Display "Happy Independence Day" after the countdown
+                    setTimeout(() => {
+                      document.getElementById("countdown").innerText =
+                        "Happy Independence Day!";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
